8a45a11a78d0f12869086f5b97030290c8414ab0
GaneshHegde1029 <ganeshhegde7182@gmail.com>
2025-06-22 12:21:36 +0530

    Delete FactoryMethodPatternExample directory

--- FactoryMethodPatternExample/ExcelDocumentFactory.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethodPatternExample;
-
-public class ExcelDocumentFactory extends DocumentFactory {
-    public Document createDocument() {
-        return new ExcelDocument();
-    }
-}
-
