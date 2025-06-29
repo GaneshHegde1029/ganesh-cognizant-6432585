5a1f74889d8e8b194c38ebf1f71e2477d8f0321d
GaneshHegde1029 <ganeshhegde7182@gmail.com>
2025-06-29 19:52:32 +0530

    Add JUnit Task 1 with assertions and tests

--- week2/junit-task1/src/main/java/com/ganesh/junitlab/App.java
+++ week2/junit-task1/src/main/java/com/ganesh/junitlab/App.java
@@ -1,7 +1,6 @@
 package com.ganesh.junitlab;
 
 public class App {
-
     public int add(int a, int b) {
         return a + b;
     }
@@ -11,8 +10,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        App app = new App();
-        System.out.println("Addition of 5 and 3: " + app.add(5, 3));
-        System.out.println("Subtraction of 5 and 3: " + app.subtract(5, 3));
+        System.out.println("App is working!");
     }
 }
