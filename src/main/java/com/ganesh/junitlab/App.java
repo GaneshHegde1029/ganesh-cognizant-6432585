06a4c51f2b4a97cd9f958ab60d256895f1aad43b
GaneshHegde1029 <ganeshhegde7182@gmail.com>
2025-06-29 19:25:37 +0530

    Initial commit: Add Maven JUnit Task1 project

---
+++ src/main/java/com/ganesh/junitlab/App.java
@@ -0,0 +1,15 @@
+package com.ganesh.junitlab;
+
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("App is running...");
+    }
+}
